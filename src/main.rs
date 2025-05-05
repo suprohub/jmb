d582a44f2608a7178bb485e16bd611c6000b39a2
Suprohub <suprohub@gmail.com>
2025-05-05 17:52:26 +0400

    Giga work ahead

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 pub mod assets;
 pub mod generated;
 pub mod module;
-use std::fs;
+use std::{fs::File, io::BufReader};
 
 fn main() {
     let module: module::Module =
-        serde_json::from_str(&fs::read_to_string("supe.json").unwrap()).unwrap();
+        serde_json::from_reader(BufReader::new(File::open("pr1.json").unwrap())).unwrap();
     println!("{:?}", module);
     //println!("{:?}", get_assets());
 
--- src/module.rs
+++ src/module.rs
@@ -1,11 +1,11 @@
 use heck::ToUpperCamelCase;
 use serde::{Deserialize, Serialize};
-use serde_json::Number;
 
-use crate::generated::ActionId;
+use crate::generated::{ActionId, GameValueId};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Module {
+    #[serde(deserialize_with = "deserialize_handlers")]
     pub handlers: Vec<Line>,
 }
 
@@ -20,6 +20,7 @@ pub struct Line {
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
 pub enum LineType {
+    Process,
     Function,
     Event,
 }
@@ -27,58 +28,107 @@ pub enum LineType {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Op {
     pub action: ActionId,
-    #[serde(rename = "values")]
-    pub args: Vec<Arga>,
+    pub values: Vec<NamedValue>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
-pub struct Arga {
+pub struct NamedValue {
     name: String,
-    #[serde(deserialize_with = "deserialize_arg")]
-    value: Arg,
+    #[serde(deserialize_with = "deserialize_value")]
+    value: Value,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case", tag = "type")]
-pub enum Arg {
-    Any,
-    Array,
-    Block,
+pub enum Value {
+    Array {
+        values: Vec<Value>,
+    },
+    Block {
+        block: String,
+    },
     Enum {
         #[serde(rename = "enum", deserialize_with = "deserialize_upper_camel")]
         value: String,
     },
-    Item,
-    Location,
-    Map,
+    Item {
+        item: String,
+    },
+    Location {
+        x: f64,
+        y: f64,
+        z: f64,
+        yaw: f64,
+        pitch: f64,
+    },
     Number {
         number: Number,
     },
     Particle,
-    Potion,
-    Sound,
-    Text,
+    Potion {
+        potion: String,
+        amplifier: i16,
+        duration: i16,
+    },
+    Sound {
+        sound: String,
+        pitch: f32,
+        volume: f32,
+        variaton: String,
+        source: String,
+    },
+    Text {
+        text: String,
+        parsing: TextParsing,
+    },
     Variable {
         variable: String,
         scope: VariableScope,
     },
-    Vector,
+    Vector {
+        x: f64,
+        y: f64,
+        z: f64,
+    },
+    GameValue {
+        game_value: GameValueId,
+        selection: String,
+    },
+
+    Error,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+#[repr(u8)]
+pub enum Number {
+    Simple(f64),
+    Calc(String),
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
+#[repr(u8)]
+pub enum TextParsing {
+    Legacy,
+    Plain,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+#[repr(u8)]
 pub enum VariableScope {
     Local,
     Global,
     Save,
 }
 
-fn deserialize_arg<'de, D>(deserializer: D) -> Result<Arg, D::Error>
+fn deserialize_value<'de, D>(deserializer: D) -> Result<Value, D::Error>
 where
     D: serde::Deserializer<'de>,
 {
-    Ok(Arg::deserialize(deserializer).unwrap_or(Arg::Any))
+    Ok(Value::deserialize(deserializer).unwrap_or(Value::Error))
 }
 
 fn deserialize_upper_camel<'de, D>(deserializer: D) -> Result<String, D::Error>
@@ -87,4 +137,22 @@ where
 {
     let s: String = Deserialize::deserialize(deserializer)?;
     Ok(s.to_upper_camel_case())
+}
+
+fn deserialize_handlers<'de, D>(deserializer: D) -> Result<Vec<Line>, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let v: Vec<serde_json::Value> = Deserialize::deserialize(deserializer)?;
+    let mut lines = Vec::new();
+
+    for value in v {
+        if let Some(line) = value.as_object() {
+            let line: Line = serde_json::from_value(serde_json::Value::Object(line.clone()))
+                .map_err(serde::de::Error::custom)?;
+            lines.push(line);
+        }
+    }
+
+    Ok(lines)
 }
